e6880efad7d170c4f914fd8dbe6d82b019fd6b4b
ilya <nivaiz54@gmail.com>
2022-10-03 00:19:43 +0500

    Добавил возможность закрытие модального окна по клику вне области, а также по клавише esc

--- source/js/modal.js
+++ source/js/modal.js
@@ -3,12 +3,34 @@ const clickAddClassButtonFuction = () => {
   const addClassButton = document.querySelector('.modal-content__form');
   const addClassBody = document.querySelector('body');
   const addClassModalOverlay = document.querySelector('.modal-content');
+
   clickButtonModal.addEventListener('click', (event) => {
-  event.preventDefault();
-  addClassButton.classList.toggle('modal-content__form--open');
-  addClassBody.classList.toggle('modal-content__body--noscroll');
-  addClassModalOverlay.classList.toggle('modal-content__open');
+    event.preventDefault();
+    addClassButton.classList.toggle('modal-content__form--open');
+    addClassBody.classList.toggle('modal-content__body--noscroll');
+    addClassModalOverlay.classList.toggle('modal-content__open');
 
   })
+
+  window.addEventListener("keydown", (evt) => {
+    if (evt.keyCode === 27) {
+      if (addClassModalOverlay.classList.contains('modal-content__open')) {
+        evt.preventDefault();
+        addClassModalOverlay.classList.remove('modal-content__open');
+        addClassBody.classList.remove('modal-content__body--noscroll');
+        addClassButton.classList.remove('modal-content__form--open');
+      }
+    }
+  });
+
+  window.addEventListener('click', (e) => {
+    if (e.target === addClassModalOverlay) {
+      addClassModalOverlay.classList.remove('modal-content__open');
+      addClassButton.classList.remove('modal-content__form--open');
+
+    }
+  });
+
 }
+
 clickAddClassButtonFuction();
