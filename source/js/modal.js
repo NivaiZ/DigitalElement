684933e0d2ba14d1be15f474265ef1718e604972
ilya <nivaiz54@gmail.com>
2022-10-03 18:02:46 +0500

    работа с формой

--- source/js/modal.js
+++ source/js/modal.js
@@ -27,10 +27,44 @@ const clickAddClassButtonFuction = () => {
     if (e.target === addClassModalOverlay) {
       addClassModalOverlay.classList.remove('modal-content__open');
       addClassButton.classList.remove('modal-content__form--open');
+      addClassBody.classList.remove('modal-content__body--noscroll');
 
     }
   });
 
 }
 
+const validateInput = (item) => {
+  const reg = /[-.\w]+@([\w-]+\.)+[\w-]+/g;
+
+  if (item.value.trim() == '') {
+    return 'Fill in this field';
+  }
+  if (item.id === 'your_email' && !reg.test(item.value)) {
+
+    return 'Enter a correct email address';
+  }
+  return false;
+}
+
+const checkForm = (form) => {
+  let formObj = {};
+  formObj.name = form.querySelector('#modal__name');
+  formObj.email = form.querySelector('#modal__email');
+  formObj.message = form.querySelector('#modal__message');
+  for (let key in formObj) {
+    let span = document.createElement('span');
+    span.textContent = validateInput(formObj[key]);
+    let label = form.querySelector('[for = ' + formObj[key].id + ']');
+    if (span.textContent !== 'false' && !label.querySelector('span')) {
+      label.appendChild(span);
+    }
+  }
+
+  if (!form.querySelectorAll('label span').length) {
+    return true;
+  }
+}
 clickAddClassButtonFuction();
+validateInput();
+checkForm();
